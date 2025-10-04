25acf6fdc5b155c0bfddb6d296ac34296e872f6e
ermiasdesalegn <ermiasdesalegnn@gmail.com>
2025-10-04 17:26:35 +0300

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Form from "./Form";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
 
 // const initialItems = [
 //   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -42,86 +45,7 @@ export default function App() {
     </>
   );
 }
-function Logo() {
-  return <h1>🌴Far Away 💼</h1>;
-}
-function Form({ handleAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  // const [items, setItems] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-
-    // console.log(items);
-    handleAddItems(newItem);
-    setQuantity(1);
-    setDescription("");
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>what do you need for your trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
-  );
-}
-function PackingList({ items, handleDeleteItem, handleMarked, handleRemove }) {
-  const [sortBy, setSortBy] = useState("input");
-  let sortedItems;
-  if (sortBy === "input") sortedItems = items;
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleMarked={handleMarked}
-          />
-        ))}
-      </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">sort by input order </option>
-          <option value="description">sort by description</option>n
-          <option value="packed">sort by packed status</option>
-        </select>
-        <button onClick={handleRemove}>Clear List</button>
-      </div>
-    </div>
-  );
-}
-function Item({ item, handleDeleteItem, handleMarked }) {
+export function Item({ item, handleDeleteItem, handleMarked }) {
   return (
     <li>
       <input
---
+++ src/PackingList.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Item } from "./App";
+
+export function PackingList({
+  items,
+  handleDeleteItem,
+  handleMarked,
+  handleRemove,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            handleDeleteItem={handleDeleteItem}
+            handleMarked={handleMarked}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">sort by input order </option>
+          <option value="description">sort by description</option>n
+          <option value="packed">sort by packed status</option>
+        </select>
+        <button onClick={handleRemove}>Clear List</button>
+      </div>
+    </div>
+  );
+}
